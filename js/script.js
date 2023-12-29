8a0f4e9f65e51d13a9fe55298f2ec172d5203474
leska-ver <lyaisan.333@yandex.ru>
2023-12-29 11:08:51 +0500

    Еда

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   // -- Initialize Swiper --
-  const swiperAbout = document.querySelector(".about")//Для обёртки if
-  if (swiperAbout) {//Обёртка if. Спасение Gulp-а от null в браузере
-    const swiperDesktop = new Swiper(".mySwiper-desktop", {  
+  const swiperAbout = document.querySelector(".about") //Для обёртки if
+  if (swiperAbout) { //Обёртка if. Спасение Gulp-а от null в браузере
+    const swiperDesktop = new Swiper(".mySwiper-desktop", {
       // Дополнительные параметры
       direction: 'horizontal',
       loop: false,
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       //slidesPerView: 'auto',
-      slidesPerGroup: 2,//Одна картинка - один шаг
+      slidesPerGroup: 2, //Одна картинка - один шаг
       spaceBetween: 12,
 
       //Кнопки навигации
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
           slidesPerView: 2,
           spaceBetween: 32,
         },
-    
+
         1225: {
           slidesPerView: 2,
           spaceBetween: 35,
@@ -48,11 +48,11 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       //Бесконечное листание страниц
-      speed: 3000,//Интервал ожидания
+      speed: 3000, //Интервал ожидания
 
       autoplay: {
-        delay: 5000,//Интервал ожидания
-        disableOnInteraction: false,      
+        delay: 5000, //Интервал ожидания
+        disableOnInteraction: false,
       }
 
     });
@@ -68,77 +68,143 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       //Бесконечное листание страниц
-      speed: 3000,//Интервал ожидания
+      speed: 3000, //Интервал ожидания
 
       autoplay: {
-        delay: 5000,//Интервал ожидания
-        disableOnInteraction: false,      
+        delay: 5000, //Интервал ожидания
+        disableOnInteraction: false,
       }
     });
-  };//Обёртка if 
+  }; //Обёртка if 
 
-  
 
-  // Модальное окно//
-  const btns = document.querySelectorAll('.social__foto_js');
-  const modalOverlay = document.querySelector('.modal-overlay ');
-  const modals = document.querySelectorAll('.modal');
-  
-  // отключить включение прокрутки BODY
-  const disableScroll = function disableScroll() {
-    document.body.classList.add('disable-scroll');
-  };
-  const enableScroll = function enableScroll() {
-    document.body.classList.remove('disable-scroll');
-  };
-  // -//- отключить включение прокрутки BODY -//-
 
-  
-  btns.forEach((el) => {
-    el.addEventListener('click', (e) => {
-      let path = e.currentTarget.getAttribute('data-path');
-      const currentModal = document.querySelector(`[data-target="${path}"]`);
-      const closeBtn = currentModal.querySelector('.modal__js-close');
-      
-      modals.forEach((el) => {
-        el.classList.remove('modal--visible');
-      });
-    
-      currentModal.classList.add('modal--visible');
-      modalOverlay.classList.add('modal-overlay--visible');
+  // Модальное окно в картинках social__foto
+  const body = document.querySelector('body')
 
-      //Для modal__js-close 
-      closeBtn.focus();
-        disableScroll();//?
-    });
-  }); 
+  // Открытие модального окна, если в url указан его id
+  openModalHash()
 
-  // Реакция нажима в любом месте(выключатель окна) не удалять
-  modalOverlay.addEventListener('click', (e) => {
-    console.log(e.target);
-    
-    if (e.target == modalOverlay) {
-      modalOverlay.classList.remove('modal-overlay--visible');
-      modals.forEach((el) => {
-        el.classList.remove('modal--visible');
+  function openModalHash() {
+    if (window.location.hash) {
+      const hash = window.location.hash.substring(1)
+      const modal = document.querySelector(`#${hash}`)
+      if (modal) {
+        modal.classList.add('_show');
+        bodyLock(true)
+        closeWhenClickingOnBg(`#${hash} .modal__content`, modal);
+      }
+    }
+  }
+
+  // Закрытие модальных окон при клике по крестику
+  closeModalWhenClickingOnCross()
+
+  function closeModalWhenClickingOnCross() {
+    const modalElems = document.querySelectorAll('.modal')
+    for (let i = 0; i < modalElems.length; i++) {
+      const modal = modalElems[i];
+      const closeThisModal = modal.querySelector('.modal__close')
+      closeThisModal.addEventListener('click', () => {
+        modal.classList.remove('_show')
+        bodyLock(false)
+        resetHash()
+      })
+    }
+  }
+
+  // Закрытие модальных окон при нажатии по клавише ESC
+  closeModalWhenClickingOnESC()
+  function closeModalWhenClickingOnESC() {
+    const modalElems = document.querySelectorAll('.modal')
+    for (let i = 0; i < modalElems.length; i++) {
+      const modal = modalElems[i];
+      document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape') {
+          modal.classList.remove('_show')
+          bodyLock(false)
+          resetHash()
+        }
+      })
+    }
+  }
+
+  // Сброс id модального окна в url
+  function resetHash() {
+    const windowTop = window.pageYOffset
+    window.location.hash = ''
+    window.scrollTo(0, windowTop)
+  }
+
+  // Открытие модальных окон
+  openModal()
+
+  function openModal() {
+    const btnsOpenModal = document.querySelectorAll('[data-modal-open]');
+
+    for (let i = 0; i < btnsOpenModal.length; i++) {
+      const btn = btnsOpenModal[i];
+
+      btn.addEventListener('click', (e) => {
+        const dataBtn = btn.dataset.modalOpen;
+        const modalThatOpens = document.querySelector(`#${dataBtn}`)
+        btn.classList.add('modal-show');
+        modalThatOpens.classList.add('_show');
+        bodyLock(true)
+        closeWhenClickingOnBg(`#${dataBtn} .modal__content`, modalThatOpens);
+        window.location.hash = dataBtn
       });
     }
-  });
+  }
 
-  //Реакция нажима на Х(выключатель окна)
-  modals.forEach((el) => {
-    const closeBtn = el.querySelector('.modal__js-close');
+  // Изменение скролла у body
+  function bodyLock(con) {
+    if (con == true) {
+      body.classList.add('_lock');
+    } else if (con == false) {
+      body.classList.remove('_lock');
+    }
+  }
 
-    closeBtn.addEventListener('click', function () {
-      modalOverlay.classList.remove('modal-overlay--visible');
-      modals.forEach((el) => {
-        el.classList.remove('modal--visible');
-      });
-    });
-  });
+  // Закрытие модального окна при клике по заднему фону
+  function closeWhenClickingOnBg(itemArray, itemParent, classShow = '_show') {
+    document.addEventListener('click', (e) => {
+      let itemElems = document.querySelectorAll(itemArray)
 
-  enableScroll();//?
+      for (let i = 0; i < itemElems.length; i++) {
+        const item = itemElems[i];
+        const target = e.target,
+          itsItem = target == item || item.contains(target),
+          itemIsShow = item.classList.contains(classShow);
+
+        if (itemParent) {
+          const itsItemParent = target == itemParent || itemParent.contains(target),
+            itemParentIsShow = itemParent.classList.contains(classShow);
+          if (!itsItem && itsItemParent && itemParentIsShow) {
+            itemParent.classList.remove(classShow);
+            if (body.classList.contains('_lock')) {
+              bodyLock(false)
+            }
+            if (window.location.hash === '#' + itemParent.getAttribute('id')) {
+              resetHash()
+            }
+          }
+        } else {
+          if (!itsItem && itemIsShow) {
+            item.classList.remove(classShow);
+            if (body.classList.contains('_lock')) {
+              bodyLock(false)
+            }
+            if (window.location.hash === '#' + itemParent.getAttribute('id')) {
+              resetHash()
+            }
+          }
+        }
+
+      }
+    })
+  }
 
 
-  
-}); 
+
+});
